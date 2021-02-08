0868ca2a2675ba8adb3961b5264e030cc33be018
abiel kidane <abielkidane19@hotmail.com>
2021-02-08 17:07:57 +0100

    navbar component

--- localpalette/src/app/app.module.ts
+++ localpalette/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MaterialModule} from "./module/material/material.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     MaterialModule,
