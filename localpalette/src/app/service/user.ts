7749b8f16dc0a323a318bc4217f6b2d2784854f3
abiel kidane <abielkidane19@hotmail.com>
2021-04-12 13:52:11 +0200

    fully auth
    fult auth med alle functioner  og stylet ferdig

--- localpalette/src/app/service/user.ts
+++ localpalette/src/app/service/user.ts
@@ -1,2 +1,24 @@
 export interface User {
+    uid: string;
+    email: string;
+    displayName: string;
+    photoURL: string;
+    emailVerified: boolean;
+    
 }
+
+// export interface ProductReviews{
+//     uid:string;
+//     comments :string ;
+//     review:  string;
+//     resturanID:string 
+
+// }
+
+// export interface ProductRatings{
+//     productid:string;
+//     rating:number;
+// }
+
+// // 
+
