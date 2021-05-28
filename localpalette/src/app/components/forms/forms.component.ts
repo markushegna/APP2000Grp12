a56ea6b9e4422c41434cf63b61b6c2751478661e
abiel kidane <abielkidane19@hotmail.com>
2021-05-28 16:28:26 +0200

    contact  form

---
+++ localpalette/src/app/components/forms/forms.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { TranslateComponent } from '../translate/translate.component';
+import {FormControl, Validators} from '@angular/forms';
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.scss']
+})
+export class FormsComponent implements OnInit {
+
+  constructor( public translate:TranslateComponent) { }
+
+  ngOnInit(): void {
+  }
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+}
